2ec64188d6dd01a1500f45622c60348dec811587
Matteo <matteodinoia02@gmail.com>
2022-10-06 12:51:33 +0200

    adding teacher exercize

---
+++ 01-DFA/TreZeri.java
@@ -0,0 +1,46 @@
+public class TreZeri {
+	public static boolean scan(String s) {
+		int state = 0;
+		int i = 0;
+		while (state >= 0 && i < s.length()) {
+			final char ch = s.charAt(i++);
+			switch (state) {
+				case 0:
+					if (ch == '0')
+						state = 1;
+					else if (ch == '1')
+						state = 0;
+					else
+						state = -1;
+					break;
+				case 1:
+					if (ch == '0')
+						state = 2;
+					else if (ch == '1')
+						state = 0;
+					else
+						state = -1;
+					break;
+				case 2:
+					if (ch == '0')
+						state = 3;
+					else if (ch == '1')
+						state = 0;
+					else
+						state = -1;
+					break;
+				case 3:
+					if (ch == '0' || ch == '1')
+						state = 3;
+					else
+						state = -1;
+					break;
+			}
+		}
+		return state == 3;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(scan(args[0]) ? "OK" : "NOPE");
+	}
+}
