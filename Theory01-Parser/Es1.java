5d5ba064aa1400733b4fbe0ef9b7914df9698861
Matteo <matteodinoia02@gmail.com>
2022-11-13 16:45:11 +0100

    added exercize cap 4.3

--- Theory01-Parser/Es1.java
+++ Theory01-Parser/Es1.java
@@ -1,21 +1,25 @@
 public class Es1 extends Parser {
 	protected void S() {
 		switch (peek()) {
-			case '0': // S → 0S0
+			case '0': // S -> 0S0
 				match('0');
 				S();
 				match('0');
 				break;
-			case '1':  // S → 1S1
+			case '1':  // S -> 1S1
 				match('1');
 				S();
 				match('1');
 				break;
-			case 'c': // S → c
+			case 'c': // S -> c
 				match('c');
 				break;
 			default:
-				throw error();
+				throw guideError("S");
 		}
+	}
+
+	public static void main(String args[]){
+		new Es1().executeTest();
 	}
 }
