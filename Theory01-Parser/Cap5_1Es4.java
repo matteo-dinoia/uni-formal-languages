188ddbb86003f18ed1b3de52eeff8819ab414f13
Matteo <matteodinoia02@gmail.com>
2022-11-27 17:03:44 +0100

    adding to postfix notation SDT

--- Theory01-Parser/Cap5_1Es4.java
+++ Theory01-Parser/Cap5_1Es4.java
@@ -9,15 +9,87 @@ public class Cap5_1Es4 extends Parser{
 	}
 
 	private void E() {
+		switch(peek()){
+			case '(':
+			case '0':case '1':case '2':case '3':case '4':
+			case '5':case '6':case '7':case '8':case '9':
+				T();
+				E2();
 
+				break;
+			default:
+				throw guideError("in E");
+
+		}
+	}
+
+	private void E2() {
+		switch(peek()){
+			case '+':
+				match('+');
+				T();
+				E2();
+				System.out.print('+');
+				break;
+			case ')':
+			case '$':
+					break;
+			default:
+				throw guideError("in E2");
+
+		}
 	}
 
 	private void T() {
+		switch(peek()){
+			case '(':
+			case '0':case '1':case '2':case '3':case '4':
+			case '5':case '6':case '7':case '8':case '9':
+				F();
+				T2();
+				break;
+			default:
+				throw guideError("in E");
 
+		}
+	}
+
+	private void T2() {
+		switch(peek()){
+			case '*':
+				match('*');
+				F();
+				T2();
+				System.out.print('*');
+				break;
+			case ')':
+			case '+':
+			case '$':
+				break;
+			default:
+				throw guideError("in T2");
+
+		}
 	}
 
 	private void F() {
+		switch(peek()){
+			case '(':
+				match('(');
+				System.out.print('(');
+				E();
+				match(')');
+				System.out.print(')');
+				break;
+			case '0':case '1':case '2':case '3':case '4':
+			case '5':case '6':case '7':case '8':case '9':
+				System.out.print(peek());
+				match(peek());
+				break;
+			default:
+				throw guideError("in E");
 
+		}
 	}
 
 	public static void main(String args[]){
